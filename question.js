b766a46991479349f3b62582a18da3fd7186c19b
s-ao213 <130631690+s-ao213@users.noreply.github.com>
2023-06-19 12:23:19 +0900

    Add files via upload

---
+++ question.js
@@ -0,0 +1,35 @@
+// チェックボックスのチェックを外す関数。HTMLファイルで設定した関数名と一致させる
+function checkCLEAR()
+{
+   // labelがblank1〜blank6までのチェックボックスを、for文で回して全てチェックを外す。
+   // 「6」は、blankの数に合わせて適宜設定
+    for (i=0; i<=1000; i++) {
+        // document.myTest.elements["blank"+i].checked = false;
+        // var checkbox = document.getElementById("blank" + i);
+        // if (checkbox) {
+        //     checkbox.checked = false;
+        // }
+        var checkbox = document.getElementById("blank" + i);
+        if (checkbox !== null && checkbox !== undefined) {
+            checkbox.checked = false;
+        }
+    }
+}
+
+// チェックボックスのチェックをする関数。HTMLファイルで設定した関数名と一致させる
+function checkALL()
+{
+   // labelがblank1〜blank6までのチェックボックスを、for文で回して全てチェックする。
+   // 「6」は、blankの数に合わせて適宜設定
+    for (i=0; i<=1000; i++) {
+        // document.myTest.elements["blank"+i].checked = true;
+        // var checkbox = document.getElementById("blank" + i);
+        // if (checkbox) {
+        //     checkbox.checked = true;
+        // }
+        var checkbox = document.getElementById("blank" + i);
+        if (checkbox !== null && checkbox !== undefined) {
+            checkbox.checked = true;
+        }
+    }
+}
