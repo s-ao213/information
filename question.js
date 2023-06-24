70e7126be4f9089e985313fa562d72dfbb25bccc
s-ao213 <130631690+s-ao213@users.noreply.github.com>
2023-06-24 16:27:55 +0900

    Update question.js

--- question.js
+++ question.js
@@ -33,3 +33,11 @@ function checkALL()
         }
     }
 }
+
+const btn2 = document.getElementById('btn2');
+const btn2Text = document.getElementById('btn2-text');
+
+btn2.addEventListener('click', () => {
+  // ボタンクリックでhiddenクラスを付け外しする
+    btn2Text.classList.toggle('hidden');
+});
